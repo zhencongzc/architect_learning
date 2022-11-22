060c94c99e40d4df50fe6c0c233082e0821c93e6
zc <13699169543@163.com>
2022-11-22 15:52:25 +0800

    base

--- seata-order-8801/src/main/java/com/mashibing/seataorder8801/SeataOrder8801Application.java
+++ seata-order-8801/src/main/java/com/mashibing/seataorder8801/SeataOrder8801Application.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@MapperScan("com.mashibing.seataorder8801.mapper")
 @EnableDiscoveryClient
 @EnableFeignClients
 public class SeataOrder8801Application {
